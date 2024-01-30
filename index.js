0f058ce1bfca9e5bbb20d75f5464c88e11b385f3
alan_omam <omamalan@hotmail.it>
2024-01-30 16:28:14 +0100

    Impostato in modo tale che metta in output il nome, tiplogia del biglietto in base all'età e prezzo totale. senza stile per ora

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 
-
+let userId = document.querySelector("#userIdentity")
 let age = document.querySelector("#userAge")
 let distance = document.querySelector("#userDistance")
 let button = document.querySelector("#btn")
-let prezzo = document.querySelector(".result")
-
+let prezzo = document.querySelector("#result")
+let offerta = document.querySelector("#offer")
+let identità = document.querySelector("#identity")
 
  button.addEventListener("click",
  function(){
     let fullPrice = distance.value*0.21
     let price;
  
+    identità.innerHTML = userId.value
     if(age.value < 18){
         price = fullPrice - ((20/100) * fullPrice);
-        console.log(price);
-        prezzo.innerHTML = price;
+        prezzo.innerHTML = price.toFixed(2);
+        offerta.innerHTML="Ridotto 20%"
 
     } else if(age.value > 65){
        price = fullPrice - ((40/100) * fullPrice);
-       console.log(price);
-       prezzo.innerHTML = price;
+       prezzo.innerHTML = price.toFixed(2);
+       offerta.innerHTML="Ridotto 40%"
        
     } else{
       price = fullPrice;
-       console.log(price);
-       prezzo.innerHTML = price;
+       prezzo.innerHTML = price.toFixed(2);
+       offerta.innerHTML="Biglietto standard"
     } 
     
  }
